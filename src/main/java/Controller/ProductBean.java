030f0fa4c12cdb09ccd111c980446c5cd7378107
Kimthina Ly <kimthinaly7@gmail.com>
2026-02-24 09:56:41 +0700

    Implement Model (entity, DAO + util)

---
+++ src/main/java/Controller/ProductBean.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+
+@Named("productBean")
+@ViewScoped
+public class ProductBean implements Serializable {
+    
+}
